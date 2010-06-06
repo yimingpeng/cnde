52e499760be83bbe0388659757f9b8278eae47b6
rodrigomsc <rodrigomsc@73a504da-b1ed-d369-4cb7-8d5fb047a293>
2010-06-06 18:40:56 +0000

    Exemplo canvas

--- ED/src/br/upe/dsc/de/view/GCanvas.java
+++ ED/src/br/upe/dsc/de/view/GCanvas.java
@@ -16,22 +16,22 @@ import br.upe.dsc.de.problem.LayoutProblem;
 class GCanvas extends Canvas {
 	private static final long serialVersionUID = 1L;
 	private double[] solution;
-	//private ArrayList<LayoutMachine> machines;
+	// private ArrayList<LayoutMachine> machines;
 	private IProblem problem;
-	
+
 	public GCanvas(IProblem problem) {
 		this.problem = problem;
-		//machines = ((LayoutProblem) problem).getMachines();
+		// machines = ((LayoutProblem) problem).getMachines();
 		solution = new double[problem.getDimensionsNumber()];
 		for (int i = 0; i < solution.length; i++) {
 			solution[i] = 0.0;
 		}
 	}
-	
+
 	public void setSolution(double[] solution) {
 		this.solution = solution;
 	}
-	
+
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2D = (Graphics2D) g; // cast to 2D
@@ -40,8 +40,8 @@ class GCanvas extends Canvas {
 		// Stroke s = new BasicStroke(1);
 		// g2D.setStroke(s);
 		// g2D.setColor(Color.white);
-		
-		//IProblem problem = new LayoutProblem();
+
+		// IProblem problem = new LayoutProblem();
 		double x1, x2, y1, y2, w, h, scale;
 		int xx, yy, width, height, widthMax, heightMax, margin;
 		x1 = problem.getLowerLimit(0);
@@ -50,12 +50,12 @@ class GCanvas extends Canvas {
 		y2 = problem.getUpperLimit(1);
 		w = Math.abs(x2 - x1);
 		h = Math.abs(y2 - y1);
-		
+
 		// Calculating the positions and sizes
 		widthMax = 800;
 		heightMax = 600;
 		margin = 20; // = 20px
-		
+
 		// Resizing the image
 		width = (widthMax - margin);
 		height = (int) Math.round((width * (heightMax - margin)) / width);
@@ -66,19 +66,19 @@ class GCanvas extends Canvas {
 		scale = width / w;
 		xx = (int) Math.round(margin / 2.0);
 		yy = (int) Math.round(margin / 2.0);
-		
-		System.out.println(width + " - "+ height);
-		
+
+		System.out.println(width + " - " + height);
+
 		g2D.setColor(Color.WHITE);
 		g2D.fillRect(xx, yy, width, height);
-		
+
 		g2D.setColor(Color.LIGHT_GRAY);
 		g2D.drawRect(xx, yy, (int) Math.ceil(w * scale), (int) Math.ceil(h * scale));
-		
+
 		double x, y, p;
 		int pos;
 		LayoutMachine machine;
-		
+
 		// Updating the position of all machines
 		for (int i = 0; i < 7; i++) {
 			x = solution[((i * 3) + 0)];
@@ -87,12 +87,12 @@ class GCanvas extends Canvas {
 			pos = convertPosition(p);
 			machine = ((LayoutProblem) problem).getMachine(i);
 			machine.updatePosition(x, y, pos);
-			
-			drawRectangle(g2D, (float) machine.getX1(), (float) machine.getY1(), (float) machine.getX2(),
-				(float) machine.getY2());
+
+			drawRectangle(g2D, (float) machine.getX1(), (float) machine.getY1(), (float) machine.getWidth(),
+				(float) machine.getHeight());
 		}
 	}
-	
+
 	private int convertPosition(double position) {
 		if (position <= 0.25)
 			return 0;
@@ -102,7 +102,7 @@ class GCanvas extends Canvas {
 			return 2;
 		return 3;
 	}
-	
+
 	public void drawArc(Graphics2D g2D, int x1, int y1, int x2, int y2, int sd, int rd, int cl) {
 		Arc2D.Float arc1 = new Arc2D.Float(x1, y1, x2, y2, sd, rd, cl);
 		g2D.fill(arc1);
